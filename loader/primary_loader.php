931b7d161f377d58e67cc841de944207fa25152d
BILL BRIAN <46356585+emobrya@users.noreply.github.com>
2019-09-29 16:37:07 +0300

    Update primary_loader.php

--- loader/primary_loader.php
+++ loader/primary_loader.php
@@ -44,7 +44,7 @@ class Bootstrap {
 
         if (isset($url[2])) {
             $controller->{$url[1]}($url[2]);
-            $end_time = microtime(TRUE);
+           
         } else {
             if (isset($url[1])) {
                 $controller->{$url[1]}();
